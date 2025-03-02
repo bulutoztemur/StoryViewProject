321a97130bc397c109049448abc90991976917fa
bulut.oztemur <bulutoztemur@gmail.com>
2025-03-02 22:49:40 +0100

    Initial Commit

---
+++ StoryViewProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StoryViewProject
+//
+//  Created by bulut.oztemur on 02.03.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StoryViewProject/StoryViewProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoryViewProjectApp.swift
+//  StoryViewProject
+//
+//  Created by bulut.oztemur on 02.03.25.
+//
+
+import SwiftUI
+
+@main
+struct StoryViewProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
